c39506835c386d4ade191e382096ae828b057183
Ricardo Miqueias <uslekodracir@gmail.com>
2026-06-17 20:53:56 -0300

    Escolha de classes implementada

--- Ficha.java
+++ Ficha.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Ficha {
     public static void main(String[] args) {
 
-    Personagem perDoJogador = new Personagem();
+    Classe perDoJogador = new Classe();
 
     Scanner Usuario = new Scanner(System.in);
 
@@ -45,9 +45,39 @@ public class Ficha {
         }
     }
 
-    System.out.println("Seu personagem é um... " + raça);
+    System.out.println("Seu personagem é um... " + raça + "!!!");
 
+    System.out.println("\nAgora você irá escolher qual a sua classe!");
+    System.out.println("Ela definirá suas habilidas, pontos de vida," +
+            " pontos de mana e estilo de jogo.");
+    System.out.println("");
+    System.out.println("Pense em como você pretende jogar e digite uma das opções abaixo:");
+    System.out.println("\nBárbaro   -   Bardo   -   Ladino");
 
+    String classe = "";
+    boolean classeEscolhida = false;
 
+    while (!classeEscolhida) {
+        classe = Usuario.nextLine().trim().toLowerCase();
+
+        switch (classe) {
+            case "barbaro", "berseker", "barbarian", "barbáro", "bárbaro":
+                perDoJogador.barbaro();
+                classeEscolhida = true;
+                break;
+            case "bardo", "bard", "bardoo":
+                perDoJogador.bardo();
+                classeEscolhida = true;
+                break;
+            case "ladino", "ladinho", "Rogue", "Thief":
+                perDoJogador.ladino();
+                classeEscolhida = true;
+                break;
+            default:
+                System.out.println("Tente digitar novamente...");
+        }
+
+        System.out.println("A classe escolhida foi... " + classe + "!!!");
+    }
     }
 }
